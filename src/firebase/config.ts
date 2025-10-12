7894ff20d37076715c8fbfbb728c96050c3a8492
Ayaan Haider Syed <haiderji465@gmail.com>
2025-10-12 17:51:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3659421930-344e9",
+  "appId": "1:495829892753:web:f2fdd33a530dad2b350930",
+  "apiKey": "AIzaSyDO7-VBXoCUzhov9Id5yInbsRxH6uJ_234",
+  "authDomain": "studio-3659421930-344e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "495829892753"
+};
