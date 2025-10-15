0a2dcaa1aea149d3a987f9c261f32194d37bed0a
Ayaan Haider Syed <haiderji465@gmail.com>
2025-10-15 18:31:59 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'httpshttps',
+        protocol: 'https',
         hostname: 'maps.googleapis.com',
         port: '',
         pathname: '/**',
